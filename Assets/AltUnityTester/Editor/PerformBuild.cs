972b1c21f6152034be90011cacedd8e70afe98cf
Ru Cindrea <ru.cindrea@kone.com>
2018-04-09 19:15:49 +0300

    added build file with Android instructions

---
+++ Assets/AltUnityTester/Editor/PerformBuild.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+
+class PerformBuild {
+    private static string bundleIdentifierDefault = "fi.altom.altunitytester";
+    private static string productName = "AltUnityTesterDemo";
+    private static string companyName = "Altom";
+    private static string outputFolder = "build/";
+    private static string outputFilenameAndroidDefault = outputFolder + PerformBuild.productName + ".apk";
+    private static int buildNumber = 0;
+
+	private static void Init() {
+		string versionNumber = DateTime.Now.ToString("yyMMddHHss");
+		
+		PlayerSettings.productName = productName;
+		PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, bundleIdentifierDefault);
+		PlayerSettings.bundleVersion = versionNumber;
+		PlayerSettings.Android.bundleVersionCode = int.Parse(versionNumber);
+		PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel23;
+		PlayerSettings.Android.targetDevice = AndroidTargetDevice.ARMv7;
+		PlayerSettings.companyName = companyName;
+	}
+	
+	
+	[MenuItem("Build/Target/Android")]
+	static void AndroidDefault() {
+		Debug.Log("Starting Android build..." +  PerformBuild.productName + " : " + PlayerSettings.bundleVersion);
+        Init();
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.locationPathName = outputFilenameAndroidDefault;
+        buildPlayerOptions.scenes = new [] {"Assets/AltUnityTester/ExamplesAndTests (can be deleted)/SampleScene/AltUnityDriverTestScene.unity"};
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.Development;
+        string results = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        if (results.Length == 0)
+            Debug.Log("No Build Errors");
+        else
+	        Debug.LogError("Build Error!");
+		Debug.Log("Finished. " +  PerformBuild.productName + " : " + PlayerSettings.bundleVersion);
+    }
+ }
