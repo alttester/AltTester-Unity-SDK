0b39db5183b9e93600407788faeca4fc87a4d028
Ru Cindrea <ru.cindrea@altom.ro>
2019-02-07 09:26:18 +0200

    add AltUnityBuilder

---
+++ Assets/AltUnityTester/AltUnityDriver/Editor/AltUnityBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltUnityBuilder {
+
+	public static BuildWithAltUnityTesterFromCommandLine(string platform, string[] includedScenes, string packageName, int altunitytesterPort) {
+
+	}
+
+}
