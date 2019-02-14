76eb9c14fe2a03eb71926d15a2ce79f89dcc3031
Ru Cindrea <ru.cindrea@altom.ro>
2019-02-14 21:38:25 +0200

    add more tests and add 1.2.3 to altunityrunner in requirements

--- aws-device-farm-example/tests/test_aws_example.py
+++ aws-device-farm-example/tests/test_aws_example.py
@@ -12,25 +12,32 @@ class DeviceFarmAppiumTests(unittest.TestCase):
         desired_caps = {}
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
         self.altdriver = AltrunUnityDriver(self.driver, "android")
+        self.screenshot_folder = os.getenv('SCREENSHOT_PATH', '/tmp')
 
     @classmethod
-    def teardown_class(cls):
-        cls.altdriver.stop()
-        cls.driver.quit()
+    def teardown_class(self):
+        self.altdriver.stop()
+        self.driver.quit()
 
-    def test_devicefarm(self):
-        print("test1")
+    def test_01_wait_for_scene(self):
         self.altdriver.wait_for_current_scene_to_be("Scene 1 AltUnityDriverTestScene")
-        screenshot_folder = os.getenv('SCREENSHOT_PATH', '/tmp')
-        self.driver.save_screenshot(screenshot_folder + '/devicefarm.png')
-        sleep(5)
+        self.driver.save_screenshot(self.screenshot_folder + '/scene_loaded.png')
 
-    def test_devicefarm2(self):
-        print("test2")
-        self.altdriver.find_element("Capsule").tap()
-        screenshot_folder = os.getenv('SCREENSHOT_PATH', '/tmp')
-        self.driver.save_screenshot(screenshot_folder + '/devicefarm2.png')
-        sleep(5)
+    def test_02_find_elements(self):
+        self.altdriver.find_element('Plane')
+        self.altdriver.find_element('Capsule')
+        self.driver.save_screenshot(self.screenshot_folder + '/after_capsule_tap.png')
+
+    def test_03_tap_elements(self):
+        self.altdriver.find_element('UIButton').tap()
+        capsule_info = self.altdriver.wait_for_element_with_text('CapsuleInfo', 'UIButton clicked to jump capsule!')
+        assert capsule_info.get_text() == 'UIButton clicked to jump capsule!'
+        self.driver.save_screenshot(self.screenshot_folder + "/after_button_press.png")
+        
+        # tap capsule to make it jump
+        self.altdriver.find_element('Capsule').tap()
+        self.altdriver.wait_for_element_with_text('CapsuleInfo', 'Capsule was clicked to jump!')
+        self.driver.save_screenshot(self.screenshot_folder + "/after_capsule_press.png")
 
 # Start of script
 if __name__ == '__main__':
