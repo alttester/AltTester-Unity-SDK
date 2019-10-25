3e2508db210618927956a24249f1393b954bcaf3
Robert Poienar <robert.poienar@altom.ro>
2019-10-25 11:15:25 +0300

    Delete imported package

--- Assets/AltUnityTester/AltUnityServer/Commands/EnableDebugging.cs
+++ Assets/AltUnityTester/AltUnityServer/Commands/EnableDebugging.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Assets.AltUnityTester.AltUnityServer.Commands
 {
     class EnableDebuggingCommand : Command
