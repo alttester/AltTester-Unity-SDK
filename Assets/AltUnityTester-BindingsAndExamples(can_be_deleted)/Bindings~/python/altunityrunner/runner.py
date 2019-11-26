2bc5fbc4f2d33ca92d4ee6ce17bab1f11826c28f
Robert Poienar <robert.poienar@altom.ro>
2019-11-26 17:26:05 +0200

    Deleted click_at_coordinates

--- Assets/AltUnityTester-BindingsAndExamples(can_be_deleted)/Bindings~/python/altunityrunner/runner.py
+++ Assets/AltUnityTester-BindingsAndExamples(can_be_deleted)/Bindings~/python/altunityrunner/runner.py
@@ -122,8 +122,6 @@ class AltrunUnityDriver(object):
     def get_current_scene(self):
         return GetCurrentScene(self.socket,self.request_separator,self.request_end,self.appium_driver).execute()
 
-    def click_at_coordinates(self, x, y):
-        return ClickAtCoordinates(self.socket,self.request_separator,self.request_end).execute()
 
     def swipe(self, x_start, y_start, x_end, y_end, duration_in_secs):
         return Swipe(self.socket,self.request_separator,self.request_end,x_start,y_start,x_end,y_end,duration_in_secs).execute()
