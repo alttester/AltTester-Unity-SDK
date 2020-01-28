3b7b34ad25ef9f5a7f16ef3e320061a2a940c298
Robert Poienar <robert.poienar@altom.ro>
2020-01-28 10:42:55 +0200

    Update c# checkversioncommmand

--- Assets/AltUnityTester/AltUnityDriver/Commands/CheckServerVersion.cs
+++ Assets/AltUnityTester/AltUnityDriver/Commands/CheckServerVersion.cs
@@ -12,7 +12,7 @@
             {
                 HandleErrors(serverVersion);
             }
-            var path = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Assets\\AltUnityTester\\AltUnityDriver\\CSharpDriverVersion.txt");
+            var path = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Assets/AltUnityTester/AltUnityDriver/CSharpDriverVersion.txt");
             var driverVersion= System.IO.File.ReadAllText(path);
             if (!driverVersion.Equals(serverVersion))
                 throw new System.Exception("Mismatch version. You are using different version of server and driver. Server version: " + serverVersion + " and Driver version: " + driverVersion);
