66e949d8ef4dd4de6a23cdd011062f11294fd249
Robert Poienar <poienar.robert.adam@gmail.com>
2020-02-11 22:41:24 +0200

    Update setup.py

--- Assets/AltUnityTester-BindingsAndExamples(can_be_deleted)/Bindings~/python/setup.py
+++ Assets/AltUnityTester-BindingsAndExamples(can_be_deleted)/Bindings~/python/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 with open("altunityrunner/__version__.py") as version_file:
-   VERSION=version_file.readline().replace("VERSION = ","").replace("\"","")
+    VERSION = version_file.readline().replace("VERSION = ", "").replace("\"", "")
 setup(name='altunityrunner',
       version=VERSION,
       description='Python Binding to allow Appium tests to be run against Unity games and apps using AltUnityTester',
@@ -9,14 +9,15 @@ setup(name='altunityrunner',
       author='Altom Consulting',
       author_email='altunitytester@altom.fi',
       license='GNU General Public License v3.0',
-      packages=['altunityrunner', 
-		'altunityrunner.commands', 
-		'altunityrunner.commands.FindObjects', 
-		'altunityrunner.commands.InputActions', 
-		'altunityrunner.commands.ObjectCommands', 
-		'altunityrunner.commands.OldFindObjects', 
-		'altunityrunner.commands.UnityCommands'],
+      packages=['altunityrunner',
+                'altunityrunner.commands',
+                'altunityrunner.commands.FindObjects',
+                'altunityrunner.commands.InputActions',
+                'altunityrunner.commands.ObjectCommands',
+                'altunityrunner.commands.OldFindObjects',
+                'altunityrunner.commands.UnityCommands'],
       zip_safe=False,
       install_requires=[
-         'deprecated'
-         ])
+          'deprecated',
+          'pure-python-adb'
+      ])
