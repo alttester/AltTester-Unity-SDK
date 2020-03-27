2c148c0c982af78a34b6b99d6f55ac93a1892436
Robert Poienar <poienar.robert.adam@gmail.com>
2020-03-27 22:27:27 +0200

    Fixed python bindings to work with python 2.7 and uncommented input function

--- Assets/AltUnityTester-BindingsAndExamples(can_be_deleted)/Bindings~/python/altunityrunner/commands/get_server_version.py
+++ Assets/AltUnityTester-BindingsAndExamples(can_be_deleted)/Bindings~/python/altunityrunner/commands/get_server_version.py
@@ -7,17 +7,20 @@ import warnings
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 my_file = os.path.join(THIS_FOLDER, 'PythonServerVersion.txt')
 
+
 class GetServerVersion(BaseCommand):
 
-    def __init__(self, socket,request_separator,request_end):
-        super().__init__(socket,request_separator,request_end)
-    
+    def __init__(self, socket, request_separator, request_end):
+        super(GetServerVersion).__init__(
+            socket, request_separator, request_end)
+
     def execute(self):
-        serverVersion=self.send_data(self.create_command('getServerVersion'))
-        serverVersion=self.handle_errors(serverVersion)
-        
-        if not VERSION==serverVersion:
-            message="Version mismatch. You are using different versions of server and driver. Server version: " + serverVersion + " and Driver version: " + VERSION
+        serverVersion = self.send_data(self.create_command('getServerVersion'))
+        serverVersion = self.handle_errors(serverVersion)
+
+        if not VERSION == serverVersion:
+            message = "Version mismatch. You are using different versions of server and driver. Server version: " + \
+                serverVersion + " and Driver version: " + VERSION
             warnings.warn(message)
             super().write_to_log_file(message)
             return "Version mismatch"
