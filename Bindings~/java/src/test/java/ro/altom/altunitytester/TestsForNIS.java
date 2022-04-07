3b37f46025a46a980c915eaaea83b4d9c1257cb0
Gombos Kriszta <kriszta.gombos@altom.com>
2022-04-07 12:04:20 +0300

    tests Java

--- Bindings~/java/src/test/java/ro/altom/altunitytester/TestsForNIS.java
+++ Bindings~/java/src/test/java/ro/altom/altunitytester/TestsForNIS.java
@@ -1,6 +1,7 @@
 package ro.altom.altunitytester;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,14 +9,19 @@ import org.junit.Test;
 import ro.altom.altunitytester.AltUnityDriver.By;
 import ro.altom.altunitytester.Commands.FindObject.AltFindObjectsParams;
 import ro.altom.altunitytester.Commands.InputActions.AltMultiPointSwipeParams;
+import ro.altom.altunitytester.Commands.InputActions.AltPressKeyParams;
 import ro.altom.altunitytester.Commands.InputActions.AltScrollParams;
 import ro.altom.altunitytester.Commands.InputActions.AltSwipeParams;
+import ro.altom.altunitytester.Commands.InputActions.AltKeyDownParams;
+import ro.altom.altunitytester.Commands.InputActions.AltKeyUpParams;
 import ro.altom.altunitytester.Commands.InputActions.AltMoveMouseParams;
 import ro.altom.altunitytester.Commands.InputActions.AltTapClickCoordinatesParams;
 import ro.altom.altunitytester.Commands.ObjectCommand.AltTapClickElementParams;
 import ro.altom.altunitytester.Commands.ObjectCommand.AltGetComponentPropertyParams;
 import ro.altom.altunitytester.Commands.UnityCommand.AltLoadSceneParams;
+import ro.altom.altunitytester.UnityStruct.AltUnityKeyCode;
 import ro.altom.altunitytester.position.Vector2;
+import ro.altom.altunitytester.position.Vector3;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,7 +42,8 @@ public class TestsForNIS {
 
     @BeforeClass
     public static void setUp() throws Exception {
-        altUnityDriver = new AltUnityDriver(TestsHelper.GetAltUnityDriverHost(), TestsHelper.GetAltUnityDriverPort(),
+        altUnityDriver = new AltUnityDriver(TestsHelper.GetAltUnityDriverHost(),
+                TestsHelper.GetAltUnityDriverPort(),
                 true);
     }
 
@@ -62,18 +69,22 @@ public class TestsForNIS {
 
         AltUnityObject player = altUnityDriver.findObject(altFindObjectsParams);
         Boolean isScrolling = player
-                .getComponentProperty(new AltGetComponentPropertyParams.Builder("AltUnityNIPDebugScript",
-                        "wasScrolled").withAssembly(
-                                "Assembly-CSharp")
-                        .build(), Boolean.class);
+                .getComponentProperty(
+                        new AltGetComponentPropertyParams.Builder("AltUnityNIPDebugScript",
+                                "wasScrolled").withAssembly(
+                                        "Assembly-CSharp")
+                                .build(),
+                        Boolean.class);
         assertFalse(isScrolling);
         AltScrollParams altScrollParams = new AltScrollParams.Builder().withDuration(1).withSpeed(300)
                 .withWait(true).build();
         altUnityDriver.scroll(altScrollParams);
-        isScrolling = player.getComponentProperty(new AltGetComponentPropertyParams.Builder("AltUnityNIPDebugScript",
-                "wasScrolled").withAssembly(
-                        "Assembly-CSharp")
-                .build(), Boolean.class);
+        isScrolling = player.getComponentProperty(
+                new AltGetComponentPropertyParams.Builder("AltUnityNIPDebugScript",
+                        "wasScrolled").withAssembly(
+                                "Assembly-CSharp")
+                        .build(),
+                Boolean.class);
         assertTrue(isScrolling);
     }
 
@@ -95,7 +106,8 @@ public class TestsForNIS {
                 .withWait(true).build();
         altUnityDriver.scroll(altScrollParams);
 
-        AltFindObjectsParams altFindObjectsParamsFinal = new AltFindObjectsParams.Builder(AltUnityDriver.By.NAME,
+        AltFindObjectsParams altFindObjectsParamsFinal = new AltFindObjectsParams.Builder(
+                AltUnityDriver.By.NAME,
                 "Handle").build();
         AltUnityObject scrollbarFinal = altUnityDriver.findObject(altFindObjectsParamsFinal);
         Vector2 scrollbarPositionFinal = scrollbarFinal.getScreenPosition();
@@ -114,7 +126,8 @@ public class TestsForNIS {
         capsule.click(clickParams);
 
         Boolean wasClicked = capsule
-                .getComponentProperty(new AltGetComponentPropertyParams.Builder("AltUnityExampleNewInputSystem",
+                .getComponentProperty(new AltGetComponentPropertyParams.Builder(
+                        "AltUnityExampleNewInputSystem",
                         "wasClicked").withAssembly(
                                 "Assembly-CSharp")
                         .build(), Boolean.class);
@@ -134,11 +147,84 @@ public class TestsForNIS {
         altUnityDriver.click(clickParams);
 
         Boolean wasClicked = capsule
-                .getComponentProperty(new AltGetComponentPropertyParams.Builder("AltUnityExampleNewInputSystem",
+                .getComponentProperty(new AltGetComponentPropertyParams.Builder(
+                        "AltUnityExampleNewInputSystem",
                         "wasClicked").withAssembly(
                                 "Assembly-CSharp")
                         .build(), Boolean.class);
         assertTrue(wasClicked);
+    }
+
+    @Test
+    public void TestKeyDownAndKeyUp() throws Exception {
+        loadLevel(scene10);
+        AltFindObjectsParams altFindObjectsParams = new AltFindObjectsParams.Builder(AltUnityDriver.By.NAME,
+                "Player").build();
+        AltUnityObject player = altUnityDriver.findObject(altFindObjectsParams);
+        AltGetComponentPropertyParams altGetComponentPropertyParams = new AltGetComponentPropertyParams.Builder(
+                "UnityEngine.Transform",
+                "position").build();
+        Vector3 initialPos = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        altUnityDriver.keyDown(new AltKeyDownParams.Builder(AltUnityKeyCode.A).build());
+        altUnityDriver.keyUp(new AltKeyUpParams.Builder(AltUnityKeyCode.A).build());
+        Vector3 posUp = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(initialPos, posUp);
+        altUnityDriver.keyDown(new AltKeyDownParams.Builder(AltUnityKeyCode.D).build());
+        altUnityDriver.keyUp(new AltKeyUpParams.Builder(AltUnityKeyCode.D).build());
+        Vector3 posDown = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(posUp, posDown);
+        altUnityDriver.keyDown(new AltKeyDownParams.Builder(AltUnityKeyCode.W).build());
+        altUnityDriver.keyUp(new AltKeyUpParams.Builder(AltUnityKeyCode.W).build());
+        Vector3 posLeft = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(posDown, posLeft);
+        altUnityDriver.keyDown(new AltKeyDownParams.Builder(AltUnityKeyCode.S).build());
+        altUnityDriver.keyUp(new AltKeyUpParams.Builder(AltUnityKeyCode.S).build());
+        Vector3 posRight = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(posLeft, posRight);
+    }
+
+    @Test
+    public void TestPressKey() throws Exception {
+        loadLevel(scene10);
+        AltFindObjectsParams altFindObjectsParams = new AltFindObjectsParams.Builder(AltUnityDriver.By.NAME,
+                "Player").build();
+        AltUnityObject player = altUnityDriver.findObject(altFindObjectsParams);
+        AltGetComponentPropertyParams altGetComponentPropertyParams = new AltGetComponentPropertyParams.Builder(
+                "UnityEngine.Transform",
+                "position").build();
+        Vector3 initialPos = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        altUnityDriver.pressKey(new AltPressKeyParams.Builder(AltUnityKeyCode.A).build());
+        Vector3 posUp = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(initialPos, posUp);
+        altUnityDriver.pressKey(new AltPressKeyParams.Builder(AltUnityKeyCode.D).build());
+        Vector3 posDown = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(posUp, posDown);
+        altUnityDriver.pressKey(new AltPressKeyParams.Builder(AltUnityKeyCode.W).build());
+        Vector3 posLeft = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(posDown, posLeft);
+        altUnityDriver.pressKey(new AltPressKeyParams.Builder(AltUnityKeyCode.S).build());
+        Vector3 posRight = player
+                .getComponentProperty(altGetComponentPropertyParams,
+                        Vector3.class);
+        Assert.assertNotEquals(posLeft, posRight);
     }
 
 }
