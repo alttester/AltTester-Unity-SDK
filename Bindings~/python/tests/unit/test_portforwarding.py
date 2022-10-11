02aeb68f7081a9e17a45dbab4770d7fd95a1a14f
SandyEma9 <112495136+SandyEma9@users.noreply.github.com>
2022-10-11 14:33:59 +0300

    Delete test_portforwarding.py

--- Bindings~/python/tests/unit/test_portforwarding.py
+++
@@ -1,8 +0,0 @@
-from altunityrunner.alt_unity_port_forwarding import AltUnityPortForwarding
-
-
-class TestAltUnityPortForwarding:
-
-    def test_get_iproxy_path(self):
-        assert "overwrite" == AltUnityPortForwarding._get_iproxy_path("overwrite")
-        assert "iproxy" == AltUnityPortForwarding._get_iproxy_path("")
