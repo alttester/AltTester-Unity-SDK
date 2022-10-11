930d2925e953f79f13c704b43c45a07a78988c5d
SandyEma9 <112495136+SandyEma9@users.noreply.github.com>
2022-10-11 15:11:31 +0300

    Delete log_notification_result.py

--- Bindings~/python/altunityrunner/commands/Notifications/log_notification_result.py
+++
@@ -1,7 +0,0 @@
-
-class LogNotificationResult:
-
-    def __init__(self, message, stack_trace, type):
-        self.message = message
-        self.stack_trace = stack_trace
-        self.type = type
