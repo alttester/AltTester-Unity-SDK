4b7cc6885e82983d526dc7decee895b018c1baf3
Sandy <sandrina.sere@altom.com>
2022-11-09 12:28:29 +0200

    resolve pipeline error

--- Assets/AltTester/AltServer/Commands/InputActions/AltTapElementCommand.cs
+++ Assets/AltTester/AltServer/Commands/InputActions/AltTapElementCommand.cs
@@ -14,7 +14,7 @@ namespace Altom.AltTester.Commands
         public override AltObject Execute()
         {
             AltRunner._altRunner.ShowClick(new UnityEngine.Vector2(CommandParams.altObject.GetScreenPosition().x, CommandParams.altObject.GetScreenPosition().y));
-            UnityEngine.GameObject gameObject = AltRunner.GetGameObject(CommandParams.altObject);
+            UnityEngine.GameObject gameObject = AltRunner.GetGameObject(CommandParams.altObject.id);
 
             InputController.TapElement(gameObject, CommandParams.count, CommandParams.interval, onFinish);
 
