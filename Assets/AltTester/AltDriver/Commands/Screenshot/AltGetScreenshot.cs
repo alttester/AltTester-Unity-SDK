03dc88cf696953759a50d33c0b48a5bda3e6a34c
Sandy <sandrina.sere@altom.com>
2022-11-14 17:24:56 +0200

    add inheritance for GetScreenShot classes

--- Assets/AltTester/AltDriver/Commands/Screenshot/AltGetScreenshot.cs
+++ Assets/AltTester/AltDriver/Commands/Screenshot/AltGetScreenshot.cs
@@ -11,11 +11,12 @@ namespace Altom.AltDriver.Commands
     {
         AltGetScreenshotParams cmdParams;
 
+
         public AltGetScreenshot(IDriverCommunication commHandler, AltVector2 size, int screenShotQuality) : base(commHandler)
         {
             cmdParams = new AltGetScreenshotParams(size, screenShotQuality);
         }
-        public AltTextureInformation Execute()
+        public virtual AltTextureInformation Execute()
         {
             CommHandler.Send(cmdParams);
 
@@ -26,28 +27,23 @@ namespace Altom.AltDriver.Commands
             byte[] decompressedImage = DecompressScreenshot(imageData.compressedImage);
             return new AltTextureInformation(decompressedImage, imageData.scaleDifference, imageData.textureSize);
         }
+        
     }
 
 
-    public class AltGetHighlightObjectScreenshot : AltBaseCommand
+    public class AltGetHighlightObjectScreenshot : AltGetScreenshot
     {
 
         AltHighlightObjectScreenshotParams cmdParams;
 
-        public AltGetHighlightObjectScreenshot(IDriverCommunication commHandler, int id, AltColor color, float width, AltVector2 size, int screenShotQuality) : base(commHandler)
+        public AltGetHighlightObjectScreenshot(IDriverCommunication commHandler, int id, AltColor color, float width, AltVector2 size, int screenShotQuality) : base(commHandler, size, screenShotQuality)
         {
             cmdParams = new AltHighlightObjectScreenshotParams(id, color, width, size, screenShotQuality);
         }
 
-        public AltTextureInformation Execute()
+        public override AltTextureInformation Execute()
         {
-            CommHandler.Send(cmdParams);
-            var data = CommHandler.Recvall<string>(cmdParams);
-            ValidateResponse("Ok", data);
-
-            var imageData = CommHandler.Recvall<AltGetScreenshotResponse>(cmdParams);
-            byte[] decompressedImage = DecompressScreenshot(imageData.compressedImage);
-            return new AltTextureInformation(decompressedImage, imageData.scaleDifference, imageData.textureSize);
+           return base.Execute();
         }
     }
 
@@ -56,7 +52,6 @@ namespace Altom.AltDriver.Commands
     {
         AltHighlightObjectFromCoordinatesScreenshotParams cmdParams;
 
-
         public AltGetHighlightObjectFromCoordinatesScreenshot(IDriverCommunication commHandler, AltVector2 coordinates, AltColor color, float width, AltVector2 size, int screenShotQuality) : base(commHandler)
         {
             cmdParams = new AltHighlightObjectFromCoordinatesScreenshotParams(coordinates, color, width, size, screenShotQuality);
@@ -64,6 +59,7 @@ namespace Altom.AltDriver.Commands
         public AltTextureInformation Execute(out AltObject selectedObject)
         {
             CommHandler.Send(cmdParams);
+
             selectedObject = ReceiveAltObject(cmdParams);
             if (selectedObject != null && selectedObject.name.Equals("Null") && selectedObject.id == 0)
             {
@@ -76,7 +72,7 @@ namespace Altom.AltDriver.Commands
             var imageData = CommHandler.Recvall<AltGetScreenshotResponse>(cmdParams);
             byte[] decompressedImage = DecompressScreenshot(imageData.compressedImage);
             return new AltTextureInformation(decompressedImage, imageData.scaleDifference, imageData.textureSize);
-
+    
         }
     }
 
