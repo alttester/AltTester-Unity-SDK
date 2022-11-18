a88b48df93a357fd0531ac0d583dc63c7be7f1a5
Robert Dezmerean <112888041+robert-altom@users.noreply.github.com>
2022-11-18 13:50:16 +0200

    Validations for port and host from the AltDialog don't work (#986)
    * Fix issue with validations in AltDialog.cs
    
    * Remove commented code
    
    * Remove unused import
    
    * Remove logs
    
    * Update python job
    
    * Update requirements-dev.txt
    
    * Update python job
    
    * Fix java tests
    
    * Update requirements-dev.txt
    
    * Remove TestAltPortForwarding.cs
    
    * Remove AltPortForwarding from python package
    
    * Update python job
    
    * Fix bug in TestsHelper.java
    
    * Remove unused requirement
    
    * Update python job
    
    * Update WebSocketClientCommunication.cs
    
    * Update csharp editor job
    
    * Update csharp editor job
    
    * Update WebSocketClientCommunication.cs
    
    * Update WebSocketClientCommunication.cs

--- Bindings~/python/alttester/__init__.py
+++ Bindings~/python/alttester/__init__.py
@@ -4,5 +4,4 @@ from alttester.by import By
 from alttester.playerpref import PlayerPrefKeyType
 from alttester.keycode import AltKeyCode
 from alttester.logging import AltLogLevel, AltLogger
-from alttester.portforwarding import AltPortForwarding
 from alttester.exceptions import *
