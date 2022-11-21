4647a9ef11e58f807149051bedb837eaeb891cf7
Reich Edvin <edvin.reich@altom.com>
2022-11-21 14:27:14 +0200

    Update desktop regex

--- Assets/AltTester/Editor/AltTesterEditorWindow.cs
+++ Assets/AltTester/Editor/AltTesterEditorWindow.cs
@@ -207,14 +207,14 @@ namespace Altom.AltTesterEditor
                     System.Text.RegularExpressions.Regex regex = null;
                     if (UnityEngine.SystemInfo.operatingSystemFamily == UnityEngine.OperatingSystemFamily.Windows)
                     {
-                        regex = new System.Text.RegularExpressions.Regex(@"https://altom.com/app/uploads/AltTester/desktop/AltTesterDesktop(.*?).exe");
+                        regex = new System.Text.RegularExpressions.Regex(@"https://altom.com/app/uploads/AltTester/desktop/AltTesterDesktop[\w\.]*.exe");
 
                     }
                     else if (UnityEngine.SystemInfo.operatingSystemFamily == UnityEngine.OperatingSystemFamily.MacOSX)
                     {
-                        regex = new System.Text.RegularExpressions.Regex(@"https://altom.com/app/uploads/AltTester/desktop/AltTesterDesktop(.*?).dmg");
+                        regex = new System.Text.RegularExpressions.Regex(@"https://altom.com/app/uploads/AltTester/desktop/AltTesterDesktop[\w\.]*.dmg");
                     }
-
+                    
                     System.Text.RegularExpressions.Match match = regex.Match(textReceived);
                     if (match.Success)
                     {
