47d693fe9266305c06f7b13e6ece4b0432c12811
Sandy <sandrina.sere@altom.com>
2023-01-24 13:02:56 +0200

    try googlejavaformat

--- Bindings~/java/src/test/java/com/alttester/TestsSampleScene5.java
+++ Bindings~/java/src/test/java/com/alttester/TestsSampleScene5.java
@@ -61,7 +61,7 @@ public class TestsSampleScene5 extends BaseTest {
 
         @Test
         // Test Keyboard button press
-        public void TestCameraMovement() throws InterruptedException {
+        public void TestCameraMovement() {
 
                 AltFindObjectsParams altFindObjectsParameters2 = new AltFindObjectsParams.Builder(
                                 AltDriver.By.NAME, "Player1").build();
