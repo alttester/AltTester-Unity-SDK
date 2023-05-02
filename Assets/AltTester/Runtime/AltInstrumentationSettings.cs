3d09b4e3fd7dc110d1ecb8b62ad60a337d2243cb
Robert Dezmerean <robert.dezmerean@altom.com>
2023-05-02 22:29:50 +0300

    Update AltInstrumentationSettings.cs

--- Assets/AltTester/Runtime/AltInstrumentationSettings.cs
+++ Assets/AltTester/Runtime/AltInstrumentationSettings.cs
@@ -11,26 +11,26 @@ namespace AltTester.AltTesterUnitySDK
         /// <summary>
         /// Gets or sets the host that the instrumented Unity App will connect to.
         /// </summary>
-        public string AltServerHost { get; set; } = "127.0.0.1";
+        public string AltServerHost = "127.0.0.1";
 
         /// <summary>
         /// Gets or sets the port that the instrumented Unity App will connect to.
         /// </summary>
-        public int AltServerPort { get; set; } = 13000;
+        public int AltServerPort = 13000;
 
         /// <summary>
         /// Gets or sets the name of the app that the instrumented Unity App will use as a unique identifier.
         /// </summary>
-        public string AppName { get; set; } = "__default__";
+        public string AppName = "__default__";
 
         /// <summary>
         /// Gets or sets a value indicating whether to show where an action happens on the screen (e.g. swipe or click).
         /// </summary>
-        public bool InputVisualizer { get; set; } = true;
+        public bool InputVisualizer = true;
 
         /// <summary>
         /// Gets or sets a value indicating whether to display the AltTester popup in the instrumented Unity App.
         /// </summary>
-        public bool ShowPopUp { get; set; } = true;
+        public bool ShowPopUp = true;
     }
 }
