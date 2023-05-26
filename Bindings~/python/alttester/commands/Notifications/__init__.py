ec3e50ff2e854b156a6c56372af53842fd04a847
Robert Poienar <robert.poienar@altom.com>
2023-05-26 09:31:17 +0300

    Added copyright to every file

--- Bindings~/python/alttester/commands/Notifications/__init__.py
+++ Bindings~/python/alttester/commands/Notifications/__init__.py
@@ -1,3 +1,19 @@
+﻿"""
+    Copyright(C) 2023  Altom Consulting
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from alttester.commands.Notifications.base_notification_callbacks import BaseNotificationCallbacks
 from alttester.commands.Notifications.load_scene_notification_result import LoadSceneNotificationResult
 from alttester.commands.Notifications.log_notification_result import LogNotificationResult
