e979661d7a20aadbef089838f06d2aa3dfe772cc
Robert Dezmerean <robert.dezmerean@altom.com>
2023-08-16 18:32:57 +0300

    Update IOSProxyFinder.cs

--- Assets/AltTester/Runtime/AltDriver/Proxy/IOSProxyFinder.cs
+++ Assets/AltTester/Runtime/AltDriver/Proxy/IOSProxyFinder.cs
@@ -38,38 +38,7 @@ namespace AltTester.AltTesterUnitySDK.Driver.Proxy
                 return null;
             }
 
-            CultureInfo ci = new CultureInfo("en-US");
-            string[] subs = result.Split(';');
-            foreach (var sub in subs)
-            {
-                var rule = sub.Trim();
-
-                if (rule.ToUpper() == "DIRECT") {
-                    return null;
-                }
-
-                if (rule.StartsWith("PROXY ", false, ci) || rule.StartsWith("SOCKS ", false, ci))
-                {
-                    return "http://" + rule.Substring(5).Trim();
-                }
-
-                if (rule.StartsWith("SOCKS4 ", false, ci) || rule.StartsWith("SOCKS5 ", false, ci))
-                {
-                    return "http://" + rule.Substring(6).Trim();
-                }
-
-                if (sub.StartsWith("HTTP ", false, ci))
-                {
-                    return "http://" + rule.Substring(4).Trim();
-                }
-
-                if (rule.StartsWith("HTTPS ", false, ci))
-                {
-                    return "https://" + rule.Substring(5).Trim();
-                }
-            }
-
-            return null;
+            return result;
         }
     }
 }
