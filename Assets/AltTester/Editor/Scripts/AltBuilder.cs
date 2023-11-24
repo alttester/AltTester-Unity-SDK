67545d5fe4b206f4d7f9dc1a060edc9c62fffb39
Robert Poienar <robert.poienar@altom.com>
2023-11-24 09:34:57 +0200

    Modified editor script to respect netstandard2.0

--- Assets/AltTester/Editor/Scripts/AltBuilder.cs
+++ Assets/AltTester/Editor/Scripts/AltBuilder.cs
@@ -393,17 +393,14 @@ namespace AltTester.AltTesterUnitySDK.Editor
                     reader.Close();
                     if (input.Contains("UNITY_INCLUDE_TESTS"))
                     {
-                        using StreamWriter writer = new StreamWriter(path, false);
+                        using (StreamWriter writer = new StreamWriter(path, false))
                         {
                             string output = input.Replace("\"includePlatforms\": [],",
                             "\"includePlatforms\": [\"Editor\"],");
                             writer.Write(output);
+                            writer.Close();
                         }
-                        writer.Close();
                     }
-
-
-
                 }
             }
         }
