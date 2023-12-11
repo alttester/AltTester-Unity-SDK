a21057d4ad8f28960f44e96d2cd906f4c17497e8
Robert Poienar <robert.poienar@altom.com>
2023-12-11 10:01:50 +0200

    Fixed more merge problems

--- Assets/AltTester/Runtime/Communication/CommandHandler.cs
+++ Assets/AltTester/Runtime/Communication/CommandHandler.cs
@@ -74,6 +74,11 @@ namespace AltTester.AltTesterUnitySDK.Communication
                     handleNotifications(cmdParams);
                     return;
                 }
+                if (cmdParams.commandName == "AppId")
+                {
+                    handleAppId(cmdParams);
+                    return;
+                }
 
                 var type = getCommandType((string)cmdParams.commandName);
                 var commandParams = JsonConvert.DeserializeObject(data, type, jsonSerializerSettings) as CommandParams;
@@ -99,6 +104,16 @@ namespace AltTester.AltTesterUnitySDK.Communication
                         this.Send(response);
                     }
                 });
+        }
+        public void handleAppId(CommandParams cmdParams)
+        {
+            if (cmdParams.commandName == "AppId")
+            {
+                if (this.OnAppConnect != null)
+                {
+                    this.OnAppConnect.Invoke(cmdParams.driverId);
+                }
+            }
         }
 
         public void handleNotifications(CommandParams cmdParams)
