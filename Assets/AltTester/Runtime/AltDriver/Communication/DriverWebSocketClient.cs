ea39fe4da8fa65229656a1ebcb6c5dbfadcb00c7
Robert Dezmerean <robert.dezmerean@altom.com>
2024-01-21 21:29:16 +0200

    Update DriverWebSocketClient.cs

--- Assets/AltTester/Runtime/AltDriver/Communication/DriverWebSocketClient.cs
+++ Assets/AltTester/Runtime/AltDriver/Communication/DriverWebSocketClient.cs
@@ -132,7 +132,7 @@ namespace AltTester.AltTesterUnitySDK.Driver.Communication
             // TODO: Remove this after the issue is fixed in AltWebSocketSharp
             if (this.driverType == "Desktop")
             {
-                this.wsClient.WaitTime = System.TimeSpan.FromSeconds(0.1);
+                this.wsClient.WaitTime = System.TimeSpan.FromSeconds(0.3);
             }
 
             string proxyUri = new ProxyFinder().GetProxy(string.Format("http://{0}:{1}", this.host, this.port), this.host);
