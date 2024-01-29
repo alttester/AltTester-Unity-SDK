d8f50b8e3a66de4c8b1143d42959368f87574668
Mihai Cuc <mihai.cuc@altom.com>
2024-01-29 15:28:37 +0200

    WIP c#

--- Assets/Examples/Test/Editor/Driver/TestBase.cs
+++ Assets/Examples/Test/Editor/Driver/TestBase.cs
@@ -19,15 +19,32 @@ using AltTester.AltTesterUnitySDK.Driver;
 using AltTester.AltTesterUnitySDK.Driver.Logging;
 using AltTester.AltTesterUnitySDK.Driver.Tests;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+using System;
+using System.IO;
+
 
 public class TestBase
 {
     protected AltDriver altDriver;
     protected string sceneName;
-
-    [OneTimeSetUp]
+    private static bool setupExecuted = false;
+    protected RemoteWebDriver driver;   
+    [OneTimeSetUp] 
     public void SetUp()
     {
+        if (!setupExecuted && (Environment.GetEnvironmentVariable("RUN_WEBGL_IN_BROWSERSTACK") == "true")){
+        DriverOptions capability = new OpenQA.Selenium.Chrome.ChromeOptions();
+            capability.BrowserVersion = "latest";
+            capability.AddAdditionalOption("bstack:options", capability);
+            driver = new RemoteWebDriver(
+            capability
+            );
+            driver.Navigate().GoToUrl("http://localhost:8360/index.html");
+            setupExecuted = true;
+        }
         altDriver = TestsHelper.GetAltDriver();
         DriverLogManager.SetMinLogLevel(AltLogger.Console, AltLogLevel.Info);
         DriverLogManager.SetMinLogLevel(AltLogger.Unity, AltLogLevel.Info);
@@ -41,9 +58,9 @@ public class TestBase
 
     [SetUp]
     protected void LoadLevel()
-    {
+    {   
+        driver.Manage().Window.Maximize();
         altDriver.ResetInput();
-
         altDriver.SetCommandResponseTimeout(60);
         altDriver.LoadScene(this.sceneName, true);
     }
