53f48b2ccb29b3ba4aa2e7e0076a8f2a2a606294
Robert Poienar <robert.poienar@altom.com>
2024-03-14 09:54:09 +0200

    Set appId to null when restarting the connection

--- Assets/AltTester/Runtime/UI/AltDialog.cs
+++ Assets/AltTester/Runtime/UI/AltDialog.cs
@@ -283,7 +283,7 @@ namespace AltTester.AltTesterUnitySDK.UI
         private void onRestartButtonPress()
         {
             Debug.Log("onRestartButtonPress | Method started");
-            // appId = null;
+            appId = null;
 
             responseCode = 0;
             validateFields();
