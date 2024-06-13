fc8eb812e46759f495bd960d05b23ae84fcd5692
Kriszta Gombos <kriszta.gombos@altom.com>
2024-06-13 19:17:32 +0300

    fixed linting

--- Assets/AltTester/Runtime/AltDriver/Communication/GetConnectedAppDriver.cs
+++ Assets/AltTester/Runtime/AltDriver/Communication/GetConnectedAppDriver.cs
@@ -19,8 +19,6 @@ namespace AltTester.AltTesterUnitySDK.Driver.Communication
 {
     public class GetConnectedAppDriver : BaseDriver
     {
-        private new string path = "/altws/connected-app";
-
         public GetConnectedAppDriver(string path) : base(path)
         {
         }
