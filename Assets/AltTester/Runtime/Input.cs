254b6c4001197699168f72cbf54d4bc4399e5e64
Kriszta Gombos <kriszta.gombos@altom.com>
2024-06-18 16:33:50 +0300

    formatting

--- Assets/AltTester/Runtime/Input.cs
+++ Assets/AltTester/Runtime/Input.cs
@@ -14,7 +14,6 @@
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
-
 #if ALTTESTER && ENABLE_LEGACY_INPUT_MANAGER
 
 using System;
