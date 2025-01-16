dfa0c9e768cb9641a23b709e45b274edd6c7007c
gomboskriszta15 <112488760+gomboskriszta15@users.noreply.github.com>
2025-01-16 10:52:19 +0200

    Update copyright to 2025 (#1735)
    * Update copyright to 2025
    
    * deleted file with no .cs extension

--- Bindings~/java/src/main/java/com/alttester/Commands/ObjectCommand/AltSetTextParams.java
+++ Bindings~/java/src/main/java/com/alttester/Commands/ObjectCommand/AltSetTextParams.java
@@ -1,5 +1,5 @@
 /*
-    Copyright(C) 2024 Altom Consulting
+    Copyright(C) 2025 Altom Consulting
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
