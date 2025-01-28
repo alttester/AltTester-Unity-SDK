ed3aa6f16f0dc6c0c450b8cfd42c010b6e786fb5
RobertPoienar <44575813+RobertPoienar@users.noreply.github.com>
2025-01-28 13:25:26 +0200

    1747 reconnect c driver on receiving close message 1006 (#1748)
    * Driver reconnects after receiving 1006
    
    * Driver reconects when receives 1006

--- Assets/AltTester/Runtime/AltDriver/Communication/DriverCommunicationHandler.cs
+++ Assets/AltTester/Runtime/AltDriver/Communication/DriverCommunicationHandler.cs
@@ -54,6 +54,9 @@ namespace AltTester.AltTesterUnitySDK.Driver.Communication
         private readonly string driverType;
         private static readonly object _lock = new object();
 
+        bool retryConnection = false;
+
+
         private int commandTimeout = 60;
         private float delayAfterCommand = 0;
         private bool websocketClosedCalled = false;
@@ -91,11 +94,20 @@ namespace AltTester.AltTesterUnitySDK.Driver.Communication
             };
             this.wsClient.OnCloseEvent += (sender, e) =>
             {
+                logger.Info($"Driver disconnected {e.Reason}-{e.Code}. Clean: {e.WasClean}");
+
+                if (e.Code == 1006 && !retryConnection && !websocketClosedCalled)
+                {
+                    retryConnection = true;
+                    Connect();
+                }
                 websocketClosedCalled = true;
+
             };
 
             this.wsClient.Connect();
             websocketClosedCalled = false;
+            retryConnection = false;
 
         }
 
