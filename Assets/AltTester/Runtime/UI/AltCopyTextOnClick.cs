1c3679bbc1bc66808bed20aadf536eb9ec4229b5
Robert Poienar <robert.poienar@altom.com>
2025-11-21 10:07:13 +0200

    Fix problem with consoleLogViewer define

--- Assets/AltTester/Runtime/UI/AltCopyTextOnClick.cs
+++ Assets/AltTester/Runtime/UI/AltCopyTextOnClick.cs
@@ -1,15 +1,15 @@
 using System.Text.RegularExpressions;
+using AltTester.AltTesterUnitySDK.InputModule;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
 public class AltCopyTextOnClick : MonoBehaviour, IPointerClickHandler
 {
     public Text Text;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        AltConsoleLogViewer.Instance.ShowClipboardNotification(AltConsoleLogViewer.GetMousePosition());
+        AltConsoleLogViewer.Instance.ShowClipboardNotification(InputMisc.GetMousePosition());
 
 #if UNITY_WEBGL && !UNITY_EDITOR
     AltConsoleLogViewer.Instance.Copy(Regex.Replace(Text.text, "<.*?>", string.Empty));
