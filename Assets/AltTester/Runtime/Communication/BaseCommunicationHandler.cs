f0c472dc855cebccad4f1f71d8d45ee02d61741f
andrei.on1ca <andrei.onica@altom.com>
2026-03-26 12:56:45 +0200

    Fix: use standard WebSocket client in Editor when WebGL platform is selected

--- Assets/AltTester/Runtime/Communication/BaseCommunicationHandler.cs
+++ Assets/AltTester/Runtime/Communication/BaseCommunicationHandler.cs
@@ -62,7 +62,7 @@ namespace AltTester.AltTesterUnitySDK.Communication
         }
         public void Init(string path, CommunicationDisconnectHandler OnDisconnect)
         {
-#if UNITY_WEBGL
+#if UNITY_WEBGL && !UNITY_EDITOR
                 this.wsClient = new WebGLRuntimeWebSocketClient(this.host, this.port, path, this.appName, this.platform, this.platformVersion, this.deviceInstanceId, this.appId);
 #else
             this.WsClient = new RuntimeWebSocketClient(this.Host, this.Port, path, this.AppName, this.Platform, this.PlatformVersion, this.DeviceInstanceId, this.AppId);
